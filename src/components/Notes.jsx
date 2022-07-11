a0fbbc6458c3a50559656087848976aed436db92
Khan Mohd Zahid Raza <108551410+Mr-Zahid@users.noreply.github.com>
2022-07-11 21:06:20 +0530

    Add files via upload

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Zara Sa Lyrics | Jannat</h1>
+      <p>
+        Zara si dil mein de jaga tu Zara sa apna le bana Zara sa khawbon mein
+        saja tu Zara sa yaadhon mein basa
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
